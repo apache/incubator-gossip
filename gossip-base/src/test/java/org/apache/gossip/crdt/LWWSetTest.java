4116f7b0ab4639286498f3ff134989bb0c431b30
Maxim Rusak <mak-rusak@yandex.ru>
2017-06-26 03:34:23 +0300

    GOSSIP-64 Implement Max-Change-Sets

--- gossip-base/src/test/java/org/apache/gossip/crdt/LWWSetTest.java
+++ gossip-base/src/test/java/org/apache/gossip/crdt/LWWSetTest.java
@@ -20,69 +20,26 @@ package org.apache.gossip.crdt;
 import org.apache.gossip.manager.Clock;
 import org.apache.gossip.manager.SystemClock;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class LWWSetTest {
+public class LWWSetTest extends AbstractCRDTStringSetTest<LWWSet<String>> {
   static private Clock clock = new SystemClock();
-  private Set<Integer> sampleSet;
 
-  @Before
-  public void setup(){
-    sampleSet = new HashSet<>();
-    sampleSet.add(4);
-    sampleSet.add(5);
-    sampleSet.add(12);
+  LWWSet<String> construct(Set<String> set){
+    return new LWWSet<>(set);
+  }
+
+  LWWSet<String> construct(){
+    return new LWWSet<>();
   }
 
   @Test
-  public void setConstructorTest(){
-    Assert.assertEquals(new LWWSet<>(sampleSet).value(), sampleSet);
-  }
-
-  @Test
-  public void stressWithSetTest(){
-    Set<Integer> set = new HashSet<>();
-    LWWSet<Integer> lww = new LWWSet<>();
-    for (int it = 0; it < 100; it++){
-      LWWSet<Integer> newLww;
-      if (it % 5 == 1){
-        //deleting existing
-        Integer forDelete = set.stream().skip((long) (set.size() * Math.random())).findFirst().get();
-        newLww = lww.remove(forDelete);
-        Assert.assertEquals(lww.value(), set); // check old version is immutable
-        set.remove(forDelete);
-      } else {
-        //adding
-        Integer forAdd = (int) (10000 * Math.random());
-        newLww = lww.add(forAdd);
-        Assert.assertEquals(lww.value(), set); // check old version is immutable
-        set.add(forAdd);
-      }
-      lww = newLww;
-      Assert.assertEquals(lww.value(), set);
-    }
-  }
-
-  @Test
-  public void equalsTest(){
-    LWWSet<Integer> lww = new LWWSet<>(sampleSet);
-    Assert.assertFalse(lww.equals(sampleSet));
-    LWWSet<Integer> newLww = lww.add(25);
-    sampleSet.add(25);
-    Assert.assertFalse(newLww.equals(lww));
-    Assert.assertEquals(new LWWSet<>(sampleSet), newLww);
-  }
-
-  @Test
-  public void valueTest() {
+  public void valueTest(){
     Map<Character, LWWSet.Timestamps> map = new HashMap<>();
     map.put('a', new LWWSet.Timestamps(1, 0));
     map.put('b', new LWWSet.Timestamps(1, 2));
@@ -95,61 +52,20 @@ public class LWWSetTest {
   }
 
   @Test
-  public void removeMissingTest(){
-    LWWSet<Integer> lww = new LWWSet<>(sampleSet);
-    lww = lww.add(25);
-    lww = lww.remove(25);
-    Assert.assertEquals(lww.value(), sampleSet);
-    lww = lww.remove(25);
-    lww = lww.add(25);
-    sampleSet.add(25);
-    Assert.assertEquals(lww.value(), sampleSet);
-  }
-
-  @Test
-  public void stressMergeTest(){
-    // in one-process context, add, remove and merge operations of lww are equal to operations of Set
-    // we've already checked it. Now just check merge
-    Set<Integer> set1 = new HashSet<>(), set2 = new HashSet<>();
-    LWWSet<Integer> lww1 = new LWWSet<>(), lww2 = new LWWSet<>();
-
-    for (int it = 0; it < 100; it++){
-      Integer forAdd = (int) (10000 * Math.random());
-      if (it % 2 == 0){
-        set1.add(forAdd);
-        lww1 = lww1.add(forAdd);
-      } else {
-        set2.add(forAdd);
-        lww2 = lww2.add(forAdd);
-      }
-    }
-    Assert.assertEquals(lww1.value(), set1);
-    Assert.assertEquals(lww2.value(), set2);
-    Set<Integer> mergedSet = Stream.concat(set1.stream(), set2.stream()).collect(Collectors.toSet());
-    Assert.assertEquals(lww1.merge(lww2).value(), mergedSet);
-  }
-
-  @Test
   public void fakeTimeMergeTest(){
     // try to create LWWSet with time from future (simulate other process with its own clock) and validate result
     // check remove from the future
     Map<Integer, LWWSet.Timestamps> map = new HashMap<>();
-    map.put(25, new LWWSet.Timestamps(clock.nanoTime(), clock.nanoTime() + 100000));
+    map.put(25, new LWWSet.Timestamps(clock.nanoTime(), Long.MAX_VALUE));
     LWWSet<Integer> lww = new LWWSet<>(map);
     Assert.assertEquals(lww, new LWWSet<Integer>());
     //create new LWWSet with element 25, and merge with other LWW which has remove in future
     Assert.assertEquals(new LWWSet<>(25).merge(lww), new LWWSet<Integer>());
 
     // add in future
-    map.put(25, new LWWSet.Timestamps(clock.nanoTime() + 100000, 0));
+    map.put(25, new LWWSet.Timestamps(Long.MAX_VALUE, 0));
     lww = new LWWSet<>(map);
     lww = lww.remove(25);
     Assert.assertEquals(lww, new LWWSet<>(25)); // 25 is still here
-  }
-
-  @Test
-  public void optimizeTest(){
-    Assert.assertEquals(new LWWSet<>(sampleSet).value(), sampleSet);
-    Assert.assertEquals(new LWWSet<>(sampleSet).optimize().value(), sampleSet);
   }
 }
